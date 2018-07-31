90284d6aeb739a124382fd76d1ba409b28a4c15c
Olabode <oaadimul@gmail.com>
2018-07-31 04:52:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ for (var i = 0; i < musicians.length; i++) {
 return phrases;
 }
 
-function johnLennonFacts(fac) {
+function johnLennonFacts() {
   var funfacts = [ "He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
     var i = 0
   while (funFacts.length < length) {
